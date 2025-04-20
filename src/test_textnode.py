7a48096e47cd2524de46973f37e1228fdd41c217
Mathew877 <mbrevans@gmail.com>
2025-04-19 22:08:11 -0400

    adds text_node_to_html_node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from main import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,6 +23,13 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         result = 'TextNode(This is a text node, bold, None)'
         self.assertEqual(node.__repr__(), result)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
 
 if __name__ == "__main__":
     unittest.main()
